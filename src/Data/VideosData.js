f220f10b41eb81104bc2e580d8884011cca95e10
OmarAlbek <138313268+OmarAlbek@users.noreply.github.com>
2025-07-14 23:32:44 +0300

    Add files via upload

---
+++ src/Data/VideosData.js
@@ -0,0 +1,17 @@
+const videosData = [
+  {
+    id: 1,
+    title: "Promotional Reel",
+    url: "https://www.youtube.com/embed/3Dv0H__dyRI",
+    type: "reel"
+  },
+  {
+    id: 2,
+    title: "Full Project Showcase",
+    url: "https://www.youtube.com/embed/dQw4w9WgXcQ",
+    type: "wide"
+  },
+
+];
+
+export default videosData;
---
+++ src/Data/projectsData.js
@@ -0,0 +1,30 @@
+// src/data/projectsData.js
+
+const projectsData = [
+  {
+    id: 1,
+    title: "SprayGround KW Posts",
+    image: "https://res.cloudinary.com/doacbhqcl/image/upload/v1752400397/SG_ej4cir.jpg",
+    url: "https://www.behance.net/gallery/204690591/Sprayground-Bags-Social-Media-Posts"
+  },
+  {
+    id: 2,
+    title: "Perfeccion Perfumes KW Posts",
+    image: "https://res.cloudinary.com/doacbhqcl/image/upload/v1752400410/PR_imp8qe.jpg",
+    url: "https://www.behance.net/gallery/177368561/Prefeccion-perfume-store-product-social-media-posters"
+  },
+  {
+    id: 3,
+    title: "Premiume Foods KW Posts",
+    image: "https://res.cloudinary.com/doacbhqcl/image/upload/v1752400385/PF_rnyr62.jpg",
+    url: "https://www.behance.net/gallery/209007893/Premiume-Foods-KW-Social-Media-posts"
+  },
+    {
+    id: 4,
+    title: "Petcare KW Posts",
+    image: "https://res.cloudinary.com/doacbhqcl/image/upload/v1752401529/PC_wt13yy.jpg",
+    url: "https://www.behance.net/gallery/177090271/Pet-care-vet-store-social-media-posters"
+  },
+];
+
+export default projectsData;
