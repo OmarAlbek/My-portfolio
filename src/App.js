7b37547ba3651b1fa76cf2b24d00f1e27e6cc2cb
OmarAlbek <138313268+OmarAlbek@users.noreply.github.com>
2025-07-14 23:18:03 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import './App.css';
+import './Fonts/Fonts.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Projects from './pages/Projects';
+import BottomNavBar from './Components/BottomNavBar';
+import Silk from './Components/Silk';
+import Cv from './pages/Cv';
+import Contact from './pages/Contact';
+import Videos from './pages/Videos';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="silk-background">
+        <Silk
+          speed={5}
+          scale={1}
+          color="#7250ff"
+          background="#0f0f1a"
+          noiseIntensity={1.5}
+          rotation={0}
+        />
+      </div>
+      <div className="page-content-wrapper">
+        <Router>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/videos" element={<Videos />} />
+            <Route path="/cv" element={<Cv />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+          <BottomNavBar />
+        </Router>
+      </div>
+    </div>
+  );
+}
+
+export default App;
