fd14ba1acbcc7d74cfc28e8724455e1c629ff43f
OmarAlbek <138313268+OmarAlbek@users.noreply.github.com>
2025-07-18 21:18:26 +0300

    12

--- src/Data/VideosData.js
+++ src/Data/VideosData.js
@@ -2,7 +2,7 @@ const videosData = [
   {
     id: 1,
     title: "SPRAYGROUND | SPEC OPS OFF ROAD BACKPACK",
-    url: "https://www.youtube.com/embed/C84rM7icr80k",
+    url: "https://www.youtube.com/embed/C84rM7icr80",
     type: "reel"
   },
   {
