67826b929c05784fe8b8537d081c3368af34d11f
OmarAlbek <138313268+OmarAlbek@users.noreply.github.com>
2025-07-18 23:11:04 +0300

    3

--- src/Data/VideosData.js
+++ src/Data/VideosData.js
@@ -11,6 +11,37 @@ const videosData = [
     url: "https://www.youtube.com/embed/vWhj2_PXhMs",
     type: "reel"
   },
+    {
+    id: 3,
+    title: "SPRAYGROUND | CRAZEE DAZEES BACKPACK",
+    url: "https://www.youtube.com/embed/7T7qboPpWkc",
+    type: "reel"
+  },
+  {
+    id: 4,
+    title: "SPRAYGROUND | TOKYO BUBBLE BACKPACKA",
+    url: "https://www.youtube.com/embed/K_h582zPUHo",
+    type: "reel"
+  },
+   {
+    id: 5,
+    title: "SPRAYGROUND | TROPICAL SHARK BACKPACK",
+    url: "https://www.youtube.com/embed/b7B4fgNFjk0",
+    type: "reel"
+  },
+         {
+    id: 6,
+    title: "TG Group | Grand Opening Cinematic Promo",
+    url: "https://www.youtube.com/embed/kwjWI4J5Phc",
+    type: "wide"
+  },
+     {
+    id: 7,
+    title: "SPRAYGROUND | AI SHARKGLYPHS BACKPACK",
+    url: "https://www.youtube.com/embed/bcE3KdV16QE",
+    type: "reel"
+  },
+
 ];
 
 export default videosData;
